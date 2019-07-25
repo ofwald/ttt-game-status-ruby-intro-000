9f97e0283c14a52995d9f8d86033c1a92f2abd51
Otto Wald <ofwald@gmail.com>
2019-07-25 01:15:12 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,6 +1,46 @@
+require 'pry'
 # Helper Method
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2], # Top row
+  [3,4,5],  # Middle row
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [6,4,2]
+]
+
+def won? (board)
+  WIN_COMBINATIONS.each do |win|
+    if (
+      (board[win[0]] != " " && board[win[1]] != " " && board[win[2]] != " ") &&
+      ((board[win[0]] == board[win[1]]) && (board[win[1]] == board[win[2]]))
+      )
+      return win
+    end
+  end
+  return false
+end
+
+def full?(board)
+  board.none? { |e| e == " "  }
+end
+
+def draw?(board)
+  #binding.pry
+  if ( !won?(board) && full?(board) )
+    return true
+  elsif ( !won?(board) && !full?(board) )
+    return false
+  elsif won?(board)
+    return false
+  end
+end
+
+#draw?(board = ["O", "O", " ", "X", "X", "X", " ", " ", " "])
